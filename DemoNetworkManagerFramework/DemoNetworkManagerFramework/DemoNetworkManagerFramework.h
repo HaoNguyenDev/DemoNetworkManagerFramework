a7f637432bee35cdde0a26a50a38f568214bf35f
HaoNguyen <>
2025-05-02 18:10:44 +0700

    init project

---
+++ DemoNetworkManagerFramework/DemoNetworkManagerFramework/DemoNetworkManagerFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoNetworkManagerFramework.h
+//  DemoNetworkManagerFramework
+//
+//  Created by Hao Nguyen on 2/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoNetworkManagerFramework.
+FOUNDATION_EXPORT double DemoNetworkManagerFrameworkVersionNumber;
+
+//! Project version string for DemoNetworkManagerFramework.
+FOUNDATION_EXPORT const unsigned char DemoNetworkManagerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoNetworkManagerFramework/PublicHeader.h>
+
+
