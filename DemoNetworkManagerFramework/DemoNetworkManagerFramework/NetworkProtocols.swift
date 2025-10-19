a19b63bf846ad62ea171a3eaae221fc3f30e67ba
HaoNguyen <>
2025-10-19 23:18:48 +0700

    Update framework

--- DemoNetworkManagerFramework/DemoNetworkManagerFramework/NetworkProtocols.swift
+++ DemoNetworkManagerFramework/DemoNetworkManagerFramework/NetworkProtocols.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+
 // MARK: - Define HTTP
 public enum HTTPMethod: String {
     case get = "GET"
@@ -15,19 +16,33 @@ public enum HTTPMethod: String {
     case delete = "DELETE"
 }
 
-// MARK: - Endpoint
+// MARK: - Endpoint Protocol
 public protocol Endpoint {
     var baseURL: String { get }
     var path: String { get }
     var method: HTTPMethod { get }
-    var queryParameters: [String: String]? { get }
     var headers: [String: String]? { get }
+    
+    // Query for GET and Body for POST/PUT
+    var queryItems: [URLQueryItem]? { get } // (queryParameters -> queryItems)
+    var body: Data? { get } // Body for POST/PUT/PATCH
 }
 
-// MARK: - NetworkService
-public protocol NetworkService {
-    func fetchData<T: Decodable>(endpoint: Endpoint, responseType: T.Type) async throws -> T
-    func fetchData<T: Decodable>(endpoint: Endpoint, responseType: T.Type) -> AnyPublisher<T, Error>
-    func fetchData<T: Decodable>(endpoint: Endpoint, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void)
-}
+//extension Endpoint {
+//    var queryItems: [URLQueryItem]? { nil }
+//    var headers: [String: String]? { nil }
+//    var body: Data? { nil }
+//}
 
+// MARK: - NetworkService Protocol
+public protocol NetworkServiceProtocol: AnyObject { // AnyObject for Reference Type (weak referrence)
+    
+    // Async/Await
+    func requestAsync<T: Decodable>(endpoint: Endpoint) async throws -> T
+    
+    // Combine Publisher
+    func requestPublisher<T: Decodable>(endpoint: Endpoint) -> AnyPublisher<T, Error>
+    
+    // Callback
+    func requestCallback<T: Decodable>(endpoint: Endpoint, completion: @escaping (Result<T, Error>) -> Void)
+}
